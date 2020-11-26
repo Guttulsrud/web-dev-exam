5ba07ccaa48edc9820da034b9fff98e4e7c5155f
Petlas88 <lasse.pettersen88@gmail.com>
2020-11-26 21:07:19 +0100

    Added routes after merge.

--- ASP.NETCoreWebApplication/ClientApp/src/App.js
+++ ASP.NETCoreWebApplication/ClientApp/src/App.js
@@ -22,7 +22,9 @@ export default () => {
         <BrowserRouter>
         <Layout>
             <AnimatedRoutes exitBeforeEnter initial={false}>
-                <RouteTransition  path={"/home"} children={<Home/>} />
+                <RouteTransition  path={"/"} children={<Home/>} />
+                <RouteTransition  path={"/accessories"} children={<Accessories/>} />
+                <RouteTransition  path={"/games"} children={<Games/>} />
                 <RouteTransition  path={"/game"} children={<Game/>} />
                 <RouteTransition  path={"/game2"} children={<Game2/>} />
                 <RouteTransition  path={"/game3"} children={ <Game3/>}/>
@@ -31,6 +33,18 @@ export default () => {
                 <RouteTransition  path={"/users"} children={<Users/>} />
                 <RouteTransition exact path={"/swiper"} children={ <Swipe/>} />
             </AnimatedRoutes>
+
+            {/*
+             <Route exact path="/" component={Home}/>
+                    <Route path="/games" component={Games}/>
+                    <Route path="/accessories" component={Accessories}/>
+                    <Route path="/game" component={Game}/>
+                    <Route path="/game2" component={Game2}/>
+                    <Route path="/game3" component={Game3}/>
+                    <Route path="/assets" component={Assets}/>
+                    <Route path="/login" component={Login}/>
+                    <Route exact path="/users" component={Users}/>
+            */}
 {/*
                 <Switch>
                     <Route exact path="/" component={Home}/>
