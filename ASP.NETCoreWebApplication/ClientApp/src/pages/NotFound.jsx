20f5649cb627dd05a65bfddbdd1fd83c4b781155
Petlas88 <lasse.pettersen88@gmail.com>
2020-11-27 00:03:04 +0100

    Fixed carousel chevron color. Removed SignLogo and Gradient from NotFound.jsx.

--- ASP.NETCoreWebApplication/ClientApp/src/pages/NotFound.jsx
+++ ASP.NETCoreWebApplication/ClientApp/src/pages/NotFound.jsx
@@ -6,22 +6,8 @@ import {RadialGradient} from '../components/common/RadialGradient';
 
 const NotFound = () => {
 
-    const [offset, setOffset] = useState(0);
-
-    useEffect(() => {
-        function handleScroll() {
-            setOffset(window.pageYOffset);
-        }
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
     return (
         <Section>
-            <RadialGradient/>
             <Container className={'d-flex min-vh-100'}>
                 <div className={'m-auto text-center'} >
                     <h1 style={{fontSize: '10vw', zIndex: 99, position: "relative"}}>
@@ -31,7 +17,6 @@ const NotFound = () => {
                     </h1>
                 </div>
             </Container>
-            <SignLogo style={{transform: `rotate(20deg) translateY(${offset * 0.4}px)`}}/>
         </Section>
     );
 };
