383c23c01641bbed4ed0819355291aaa75bd5de7
Petlas88 <lasse.pettersen88@gmail.com>
2020-11-27 14:56:48 +0100

    Don't mind this, just testing.

--- ASP.NETCoreWebApplication/ClientApp/src/pages/Swipe.jsx
+++ ASP.NETCoreWebApplication/ClientApp/src/pages/Swipe.jsx
@@ -4,35 +4,42 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import GameCard from '../components/pages/Home/HeaderSection/GameCard';
 import SwiperCore, {Controller, Navigation, Pagination} from 'swiper';
 
-const Swipe = () => {
-    const [controlledSwiper, setControlledSwiper] = useState(null)
 
-    SwiperCore.use([Pagination, Controller, Navigation])
+const Swipe = () => {
+
+    SwiperCore.use([Pagination, Controller, Navigation]);
+
+    const testData = [
+        {
+            title: 'Spooderman',
+            image: 'miles-morales.jpg'
+        },
+        {
+            title: 'Ratchet',
+            image: 'ratchet.jpg'
+        },
+        {
+            title: 'Aloy',
+            image: 'forbidden-west.jpg'
+        },
+        {
+            title: 'Slayer Demons',
+            image: 'demons.jpg'
+        },
+    ];
 
     return (
         <Container className={'pt-5'}>
-            <div className={'mt-5 pt-5'}>
-                <Swiper spaceBetween={50} slidesPerView={3} className={'py-5 px-5'} loop pagination controller={{ control: controlledSwiper }} navigation >
-                    <SwiperSlide>
-                        {({isNext}) => (
-                            <GameCard image={'miles-morales.jpg'} title={'Spooderman'} isActive={isNext}/>
-                        )}
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        {({isNext}) => (
-                            <GameCard image={'ratchet.jpg'} isActive={isNext}/>
-                        )}
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        {({isNext}) => (
-                            <GameCard image={'forbidden-west.jpg'} isActive={isNext}/>
-                        )}
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        {({isNext}) => (
-                            <GameCard image={'demons.jpg'} isActive={isNext}/>
-                        )}
-                    </SwiperSlide>
+            <div className={'mt-5 pt-5 '}>
+                <Swiper spaceBetween={90} slidesPerView={3} className={'py-5 px-5'} loop pagination navigation >
+                    {testData.map((slide, index) => (
+                            <SwiperSlide key={`slide${index}`} >
+                                {({isActive, isNext}) => (
+                                    <GameCard {...slide} isActive={isNext}/>
+                                )}
+                            </SwiperSlide>
+                    ))
+                    }
                 </Swiper>
             </div>
         </Container>
