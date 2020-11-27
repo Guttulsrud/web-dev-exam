b7aee72c973f29e8e6b029e1f9c6f0fb4ee733fd
eirikandreas <eirikab@gmail.com>
2020-11-27 03:06:08 +0100

    Started search input component

---
+++ ASP.NETCoreWebApplication/ClientApp/src/components/common/Search/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import {SearchInput} from "./style";
+
+
+const Search = (props) => {
+
+    return (
+        <>
+
+            <Modal
+                show={props.show}
+                onHide={props.handleClose}
+                style={{zIndex: "10000000", marginTop: "100px"}}
+
+            >
+                <SearchInput placeholder="Search games" type="text"></SearchInput>
+
+                {/*
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="Search games"
+                            aria-label="Search games"
+                            aria-describedby="basic-addon2"
+                        />
+                        <InputGroup.Append>
+                            <Button variant="outline-secondary">Button</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+
+                    */}
+
+
+
+            </Modal>
+            </>
+    )
+}
+
+export default Search;
---
+++ ASP.NETCoreWebApplication/ClientApp/src/components/common/Search/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Overlay = styled.div`
+ position: relative;
+ width: 600px;
+ height: 600px;
+ background-color: #ff00cc;
+`
+
+export const SearchInput = styled.input`
+ position: relative;
+ font-size: 2.2rem;
+ padding: 20px;
+ width: 100%;
+ border-radius: 30px;
+ border: none;
+ margin: 0;
+
+`
+
