fbfda421134c8cca6453a2b3259bef0cb9dee399
Petlas88 <lasse.pettersen88@gmail.com>
2020-11-27 18:19:22 +0100

    Added button link in video banner. Minor design changes.

--- ASP.NETCoreWebApplication/ClientApp/src/components/pages/Home/InfoSection/style.js
+++ ASP.NETCoreWebApplication/ClientApp/src/components/pages/Home/InfoSection/style.js
@@ -8,3 +8,13 @@ export const Video = styled.video`
   top: 0;
   left: 0;
 `
+
+export const VideoOverlay = styled.div`
+   height: 100%;
+   width: 100%;
+   position: absolute;
+   top: 0;
+   left: 0; 
+   background-color: #0072ecdc;
+   z-index: 10;
+`
