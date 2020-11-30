7ca229da7c6e81a87093654688e0f67ee4c67267
eirikandreas <eirikab@gmail.com>
2020-11-30 01:37:11 +0100

    Added links to footer, visual changes to footer

--- ASP.NETCoreWebApplication/ClientApp/src/components/common/AccessoriesCard/index.jsx
+++ ASP.NETCoreWebApplication/ClientApp/src/components/common/AccessoriesCard/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {Col} from "react-bootstrap";
 import {Card} from './style';
 
-const AccessoriesCard = ({id, title, description, image, price}) => {
+const AccessoriesCard = ({col, id, title, description, image, price}) => {
     return (
-        <Col lg={"6"}>
+        <Col lg={col}>
+
+
             <Card>
                 <img src={`accessories/${image}`} alt="Accessory"/>
                 Accessories
--- ASP.NETCoreWebApplication/ClientApp/src/components/layouts/Footer/Footer.jsx
+++ ASP.NETCoreWebApplication/ClientApp/src/components/layouts/Footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import {FooterBody} from './style';
+import {NavLink,Link, useLocation} from 'react-router-dom';
+import {FooterBody, SieLogo, Author, PsFigures} from './style';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import SonyLogo from "./SonyLogo";
 import Container from 'react-bootstrap/Container';
 
@@ -7,10 +11,31 @@ const Footer = () => {
     return (
       <FooterBody className={"text-white"}>
           <Container>
-              <div className={"d-flex justify-content-center"}>
-                  <a href={"http://www.sony.com/"}><SonyLogo/></a>
-              </div>
+              <Row className={"pt-5 pb-5 mt-5 mb-5"}>
+                  <Col md={"6"}>
+                      <ul className={"footer-links"}>
+                          <li><NavLink as={Link} to="/">Home</NavLink></li>
+                          <li><NavLink as={Link} to="/games">Explore Games</NavLink></li>
+                          <li> <NavLink as={Link} to="/psplus">PS Plus</NavLink></li>
+                          <li> <NavLink as={Link} to="/accessories">Accessories</NavLink></li>
+                          <li> <NavLink as={Link} to="/playstation">Playstation 5</NavLink></li>
+                      </ul>
+                  </Col>
+                  <Col md={"6"} className={"d-flex justify-content-end align-content-center align-items-center"}>
+                      <a href={"https://www.sie.com/"}><SieLogo/></a>
+                  </Col>
+
+                  <Col md={"12"} className={"d-flex justify-content-center align-content-center align-items-center"}>
+                      <PsFigures/>
+                  </Col>
+              </Row>
           </Container>
+
+                      <Author>
+                         Webutvikling 3, Eksamen, Høst 2020
+                      </Author>
+
+
       </FooterBody>
     )
 }
