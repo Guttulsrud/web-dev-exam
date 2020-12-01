6aff66fd0011ff83f5abe47cbfe3b1d07cf5b788
Martin Hallan <martin.hallan@gmail.com>
2020-12-01 13:42:22 +0100

    Working device page with breadtext and slight hack

--- ASP.NETCoreWebApplication/ClientApp/src/pages/Devices.jsx
+++ ASP.NETCoreWebApplication/ClientApp/src/pages/Devices.jsx
@@ -5,33 +5,44 @@ import {Section} from "../components/base/Section";
 import Container from "react-bootstrap/Container";
 import {SectionTitle} from "../components/base/SectionTitle";
 import AccessoriesList from "../components/common/AccessoriesList";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 
 export const Devices = () => {
-
-    const data = [
-        {
-            id: 2001,
-            title: "Playstation 5",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus posuere risus vel semper. Etiam tempus arcu non velit dignissim semper in ac turpis.",
-            backgroundImage: "ps5disk.png",
-           // gameLogo: "demonslogo.png",
-            darkTheme: false,
-        },
-        {
-            id: 2002,
-            title: "Playstation 5",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus posuere risus vel semper. Etiam tempus arcu non velit dignissim semper in ac turpis.",
-            backgroundImage: "ps5disk.png",
-            // gameLogo: "demonslogo.png",
-            darkTheme: false,
-        }]
 
     return (
         <React.Fragment>
             <Section style={{paddingTop: "60px"}}>
                 <Container>
-                    <SectionTitle>Devices</SectionTitle>
+
+                    <Row className="d-flex justify-content-between">
+
+                        <Col md={6}>
+                            <SectionTitle className="container-fluid" style={{fontSize: "84px"}}>Playstation 5</SectionTitle>
+                            <h1>Bacon ipsum dolor amet pork belly turducken pancetta, picanha cow hamburger beef ribs.
+                                Shoulder bresaola buffalo, chicken tongue pork chop fatback cupim. Strip steak pork
+                                belly tenderloin tongue picanha meatloaf ball tip short ribs sausage pork frankfurter
+                                ribeye corned beef brisket bacon. T-bone short ribs shank tenderloin bresaola.
+
+                                Bacon ipsum dolor amet pork belly turducken pancetta, picanha cow hamburger beef ribs.
+                                Shoulder bresaola buffalo, chicken tongue pork chop fatback cupim. Strip steak pork
+                                belly tenderloin tongue picanha meatloaf ball tip short ribs sausage pork frankfurter
+                                ribeye corned beef brisket bacon. T-bone short ribs shank tenderloin bresaola.
+                            </h1>
+                        </Col>
+                        <Col md={6}>
+                            <div style={{
+                                backgroundImage: "url('ps5disk.png')",
+                                width: "100%",
+                                height: "100%",
+                                position: "absolute",
+                                overflow: "visible",
+                                backgroundSize: "cover",
+                                backgroundPosition: "top", marginTop: "10vh"}} fluid></div>
+                        </Col>
+                    </Row>
                 </Container>
             </Section>
         </React.Fragment>
