9cbf463e4b2d3e75f86d9f04453579def61e9c1e
hallahallan <martin.hallan@gmail.com>
2020-12-01 23:05:52 +0100

    Revamped Device page

--- ASP.NETCoreWebApplication/ClientApp/src/components/base/SectionText.js
+++ ASP.NETCoreWebApplication/ClientApp/src/components/base/SectionText.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SectionText = styled.h1`
     position: relative;
     display: flex;
-    justify-content: ${props => (props.right ? "flex-end" : "flex-start")};
+    justify-content: ${props => (props.right ? "flex-end" : props.center ? "center" : "flex-start")};
     text-align: ${props => (props.center ? "center" : "left")};
     width: 100%;
     color: ${props => (props.white ? "#FFFFFF" : " #0070D1")};
